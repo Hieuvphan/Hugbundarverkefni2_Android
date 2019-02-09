fe2cbf02df08f900a9634920c4d41e2bb3602562
Roman <roman.rumba@hotmail.com>
2019-02-09 21:09:42 +0000

    Created api_caller for calling http  requests and created a fully working login feature

--- VeryWowChat/app/src/main/java/hi/hugbo/verywowchat/controllers/MainActivity.java
+++ VeryWowChat/app/src/main/java/hi/hugbo/verywowchat/controllers/MainActivity.java
@@ -1,13 +1,150 @@
 package hi.hugbo.verywowchat.controllers;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import hi.hugbo.verywowchat.entities.Errors;
+import hi.hugbo.verywowchat.services.API_caller;
+import okhttp3.Response;
+
+/**
+ * @Author : Róman
+ * This Activity is responsible for displaying and maitaining the login form and redirecting
+ * the user to his/her home page if he/she is logged in.
+ * */
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The login form consists of 4 widgets,
+     * and we need to have referances to them so we could work with them.
+     * the letter 'm' in front of the variable indicates that this is a member of this classes activity
+     * */
+    private TextView mLogginUserName;
+    private TextView mLogginPassword;
+    private TextView mbtnLoggin;
+    private TextView mbtnRegister;
+
+    /**
+     * The api_caller is used to send http requests to the VeryWowChat server
+     * and also it is a singleton so we are dependant on reciving a instance of it from someone else
+     * */
+    private API_caller api_caller = API_caller.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // ask teacher if this is a good solution ?
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        // -----------------------------------------------------------------------------------------
+
+        /* Shared preferences allows us to store and retrieve data in a form of a (key,value) pairs.
+           UserInfo stores 3 keys  1 = displayname , 2 = username, 3 = token
+
+           MODE_PRIVATE: File creation mode: the default mode, where the created file can only be accessed
+                         by the calling application (or all applications sharing the same user ID).
+
+           MODE_WORLD_READABLE: File creation mode: allow all other applications to have read access to the created file.
+
+           MODE_WORLD_WRITEABLE : File creation mode: allow all other applications to have write access to the created file. */
+        final SharedPreferences UserInfo = getApplicationContext().getSharedPreferences("UserInfo", MODE_PRIVATE);
+
+        // Check if the token exists in the storage if it does then we redirect the user to the HomeActivity
+        if(UserInfo.contains("token")) {
+            Intent inten = new Intent(getApplicationContext(),HomeActivity.class);
+            inten.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(inten);
+        }
+
+        // No token exists so Login form will be displayed
         setContentView(R.layout.activity_main);
+
+        // get a referance of the widgets
+        mLogginUserName = findViewById(R.id.edit_username);
+        mLogginPassword = findViewById(R.id.edit_password);
+
+        /* Since phones work way differently, its good practice like with buttons to assign
+        *  a reference to them and right away asign whatever the button needs to have like in
+         *  this case a listener. We add a listener to the button whenever its clicked it will
+         *  display a Registation form */
+        mbtnRegister = findViewById(R.id.btn_register);
+        mbtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+        /* Get a referance of the button and assign a listner to it so when ever the button is clicked
+        *  it will obtain the information from the form and pass it down to the api_caller. */
+        mbtnLoggin = findViewById(R.id.btn_login);
+        mbtnLoggin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mLogginPassword.getText().toString().isEmpty() || mLogginUserName.getText().toString().isEmpty() ){
+                    Toast.makeText(getApplicationContext(),"Please fill all the fields",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // Create a Map from the data provided by the user
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("password", mLogginPassword.getText().toString());
+                params.put("userName", mLogginUserName.getText().toString());
+
+                /* Send the HTTP request for login through the api_caller and then map the object
+                *  correctly based of the status code */
+                try {
+                    Map<String, String> result = api_caller.HttpRequest("login","POST","",params);
+                    int status = Integer.parseInt(result.get("status"));
+                    JSONArray resp_body = new JSONArray(result.get("response"));
+                    /* Talk to team if they really want to map result to object
+                     * Its possible but all of our data is inside array inside json inside array
+                      * creating billions Of enities is not such a smart thing in andriod cuz we would
+                      * be wating resources compared to using f.x JSONObject class */
+                    if(status >= 200 && status < 300 ){
+                        /* Since its a successful request we know we should recive user information
+                        *  so we store this information in the shared preferences */
+                        SharedPreferences.Editor editor = UserInfo.edit();
+                        editor.putString("username",resp_body.getJSONObject(0).get("username").toString());
+                        editor.putString("displayname",resp_body.getJSONObject(0).get("displayname").toString());
+                        editor.putString("token",resp_body.getJSONObject(0).get("token").toString());
+                        editor.commit();
+                        /* We want to clear all history and start a new Actitivity */
+                        Intent inten = new Intent(getApplicationContext(),HomeActivity.class);
+                        inten.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(inten);
+                        return;
+                    }
+                    if(status >= 400 && status < 500){
+                        String errors_msg = "";
+                        for (int i = 0; i< resp_body.getJSONObject(0).getJSONArray("errors").length(); i++) {
+                            errors_msg += resp_body.getJSONObject(0).getJSONArray("errors").getJSONObject(i).getString("message");
+                        }
+                        Toast.makeText(getApplicationContext(),errors_msg,Toast.LENGTH_LONG).show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
---
+++ VeryWowChat/app/src/main/java/hi/hugbo/verywowchat/services/API_caller.java
@@ -0,0 +1,110 @@
+package hi.hugbo.verywowchat.services;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * @Author Róman
+ * This class is responsible for performing HTTP requests to the VeryWowChat API
+ * and returning the request back to the caller functions.
+ * There should only be one instance of this class and no more.
+ * */
+public class API_caller {
+    /**
+     * We want want to ensure that there will be only a single instance of this class
+     * Mainly to prevent resource overuse since on phones resources are pretty tight
+     * */
+    private static final API_caller ourInstance = new API_caller();
+
+    /**
+     * We use the okHttp libary to make all of our HTTP Requests
+     * Example i like : https://guides.codepath.com/android/Using-OkHttp
+     * */
+    private final OkHttpClient client = new OkHttpClient();
+    private final MediaType JSON  = MediaType.parse("application/json; charset=utf-8");
+
+    /**
+     * This is used to create a singleton instance of this class, also the empty class instantiator.
+     * */
+    public static API_caller getInstance() {
+        return ourInstance;
+    }
+    private API_caller() {}
+
+    /* Usage : HttpRequest(urlEndPoint,method,token,body)
+    *    FOR : urlEndPoint is a string (Cannot be empty/null)
+    *          method is a string      (Cannot be empty/null)
+    *          token is a string       (can be null)
+    *          body is a  Map          (can be null)
+    *   After: Performs a synchronous HTTP (method) request one BASEURL/urlEndPoint and passes along the optional
+    *          token in the header under "Authorization" and the body in the RequestBody as "application/JSON UTF 8*/
+    public Map<String, String> HttpRequest(String urlEndPoint,String method,String token, Map body) throws IOException, JSONException {
+
+        /* If the body was passed then we create a request body
+        *  to send it with the HTTP request */
+        RequestBody Rbody = null;
+        if(body != null) {
+            JSONObject parameter = new JSONObject(body);
+            Rbody = RequestBody.create(JSON, parameter.toString());
+        }
+
+        /* if the token is null then we set it into a empty string */
+        if(token == null) {
+            token = "";
+        }
+
+        /* Create the HTTP Request
+        *  We always send the empty token and body its up to the server to decide if it
+        *  wants to process those things */
+        Request request = new Request.Builder()
+                .url("http://192.168.1.67:9090/"+urlEndPoint)
+                .method(method,Rbody)
+                .header("Authorization", token)
+                .addHeader("content-type", "application/json; charset=utf-8")
+                .build();
+
+        // Create a Call object and dispatch the network request synchronously
+        Response response = client.newCall(request).execute();
+
+        /* Since we dont want to pass down the whole response we create our own object
+        *  that will hold 2 things 1: status code of the response 2: the response body in json string form */
+        Map<String, String>  resp = new HashMap<String, String>();
+        JSONObject WrappedData = new JSONObject(response.body().string());
+
+        // add the status code
+        resp.put("status", String.valueOf(response.code()));
+
+        // if the status code is 204 then we know there is no content
+        if(response.code() == 204) {
+            resp.put("response",null);
+            return resp;
+        }
+
+        /* The server can throw some random stuff at us and we want to know what happens if it does */
+        if(WrappedData.has("message")){
+            resp.put("response",null);
+            return resp;
+        }
+
+        // if the status code is 200-2xx then its a success
+        if(response.code() >= 200 && response.code() < 300 ) {
+            resp.put("response",WrappedData.getJSONArray("GoodResp").toString());
+        }
+        if(response.code() >= 400 && response.code() < 500 ) {
+            resp.put("response",WrappedData.getJSONArray("BadResp").toString());
+        }
+
+        return resp;
+    }
+
+}
