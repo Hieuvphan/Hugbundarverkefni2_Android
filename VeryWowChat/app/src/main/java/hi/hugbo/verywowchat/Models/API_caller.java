a5bc3d9e7d0f263eaf00bc006a55445fa0a208cb
Roman <roman.rumba@hotmail.com>
2019-03-12 17:49:55 +0000

    Tidy upp my files for coderev  in Models

--- VeryWowChat/app/src/main/java/hi/hugbo/verywowchat/Models/API_caller.java
+++ VeryWowChat/app/src/main/java/hi/hugbo/verywowchat/Models/API_caller.java
@@ -35,7 +35,6 @@ public class API_caller {
 
     /**
      * We use the okHttp library to make all of our HTTP Requests
-     * Example i like : https://guides.codepath.com/android/Using-OkHttp
      * */
     private final OkHttpClient client = new OkHttpClient();
     private final MediaType JSON  = MediaType.parse("application/json; charset=utf-8");
@@ -74,22 +73,26 @@ public class API_caller {
      */
     public Map<String, String> HttpRequest(String urlEndPoint,String method,String token, Map body) throws IOException, JSONException {
 
-        /* If the body was passed then we create a request body
-        *  to send it with the HTTP request */
+        /*
+         *If the body was passed then we create a request body
+         *to send it with the HTTP request
+         * */
         RequestBody Rbody = null;
         if(body != null) {
             JSONObject parameter = new JSONObject(body);
             Rbody = RequestBody.create(JSON, parameter.toString());
         }
 
-        /* if the token is null then we set it into a empty string */
+        // if the token is null then we set it into a empty string
         if(token == null) {
             token = "";
         }
 
-        /* Create the HTTP Request
-        *  We always send the empty token and body its up to the server to decide if it
-        *  wants to process those things */
+        /*
+         * Create the HTTP Request
+         * We always send the empty token and body,
+         * its up to the server to decide if it wants to process those things
+         * */
         Request request = new Request.Builder()
                 .url(g_envs.getAPI_BASEURL()+urlEndPoint)
                 .method(method,Rbody)
@@ -100,39 +103,53 @@ public class API_caller {
         // Create a Call object and dispatch the network request synchronously
         Response response = client.newCall(request).execute();
 
-        /* Since we dont want to pass down the whole response we create our own object
-        *  that will hold 2 things 1: status code of the response 2: the response body in json string form */
+        /*
+         *Since we don't want to pass down the whole response we create our own object
+         *that will hold 3 things
+         *  - 1: status code of the response
+         *  - 2: the response body in json string form
+         *  - 3: the type of object we are receiving
+         **/
         Map<String, String>  resp = new HashMap<String, String>();
 
         // add the status code to the response
         resp.put("status", String.valueOf(response.code()));
-        resp.put("content-type",response.header("Content-Type")); // need this later for checking for images
+
+        // we need to know content-type for resource process such as images and files
+        resp.put("content-type",response.header("Content-Type"));
 
         // if the status code is 204 then we know there is no content
         if(response.code() == 204) {
             resp.put("response",null);
             return resp;
         }
+
         // if the response is not 204 then there is some kind of body that we need to receive
         JSONObject WrappedData = new JSONObject(response.body().string());
 
-        /* The server can throw some random stuff at us and we want to know what happens if it does
-         * We  log it's content out in LogCat but we send null to the controller */
+        /*
+         * The server can throw some random stuff at us and we want to know what happens if it does
+         * We log it's content out in LogCat but we send null to the services/controllers
+         * so they can proccess this information better
+         * */
         if(WrappedData.has("message")){
             Log.e("Api_Caller","Unexpected response from the server \n"+WrappedData.toString());
             resp.put("response",null);
             return resp;
         }
 
-        /* Our Server wraps our data in response bodies GoodResp,BadResp depending on the response
-        *  we unwrapp the data correctly */
+        /*
+         *Our Server wraps our data in response bodies GoodResp,
+         *BadResp depending on the response
+         *we un-wrap the data correctly
+         **/
 
-        /* if the status code is 200-2xx then its a success */
+        // if the status code is 200-2xx then its a success meaning wrapper is GoodResp
         if(response.code() >= 200 && response.code() < 300 ) {
             resp.put("response",WrappedData.get("GoodResp").toString());
         }
 
-        /* if the status code is 300-3xx then its a errpor */
+        // if the status code is 300-3xx then its a error meaning wrapper is BadResp
         if(response.code() >= 400 && response.code() < 500 ) {
             resp.put("response",WrappedData.get("BadResp").toString());
         }
@@ -153,9 +170,8 @@ public class API_caller {
      * @return ResourceContent
      */
     public ResourceContent HttpRequestGetResource(String urlEndPoint, String token) throws IOException {
-        /* Create the HTTP Request
-         *  We always send the empty token and body its up to the server to decide if it
-         *  wants to process those things */
+
+        // Create a HTTP REQUEST
         Request request = new Request.Builder()
                 .url(g_envs.getAPI_BASEURL()+urlEndPoint)
                 .method("GET",null)
@@ -166,6 +182,7 @@ public class API_caller {
         // Create a Call object and dispatch the network request synchronously
         Response response = client.newCall(request).execute();
 
+        // create a new instance of ResourceContent and return it to service/controller
         return  new ResourceContent(response.body().byteStream(),response.header("Content-Type"));
     }
 }
